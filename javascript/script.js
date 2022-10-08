63b07c14b15afeadd4ddd161fe669cd75b08c28d
dbutch25 <danielbutcher25@gmail.com>
2022-10-08 10:09:06 -0400

    pushing reworked js file to grab results through params

--- javascript/script.js
+++ javascript/script.js
@@ -1,23 +1,83 @@
+const app = {};
 
-const app = {}
+app.token = "ZLv4DuWOXJMDJjtoemNuEtwro"
 
-app.token = "ZLv4DuWOXJMDJjtoemNuEtwro";
 
-app.getRecords = () => {
-  const url = new URL ("https://data.cityofnewyork.us/resource/p937-wjvj.json");
+app.url = "https://data.cityofnewyork.us/resource/p937-wjvj.json"
+
+
+app.getRecords = function() {
+  
+  const url = new URL (app.url);
+ 
   url.search = new URLSearchParams({
     "$$app_token": app.token,
+    "$limit": 1,
+    house_number: "255",
+    street_name: '',
+    borough: ''
   });
 
-  fetch(url)
-    .then(result => result.json())
-    .then(data => console.log(data))
+   
+  
+    //shorthand for fetch. then then 
+  fetch(url).then((rodent) =>{
+    return rodent.json();
+  })
+  .then((jsonData) =>{
+    document.querySelector('.inspectionResults').innerHTML = '';
+    // app.displayResults(jsonData);
+
+  console.log(jsonData)
+  })
 };
 
+// For each object within the array show selected results and append to li then append all to 
+app.displayResults = function([]) {
+  [].forEach((Object) =>{
+    
+    const number = document.createElement("li");
+    number.innerText = Object.house_number
 
-//ACCORDION MENU
+    const address = document.createElement("li");
+    address.innerText = Object.street_name;
+
+    const zipCode = document.createElement("li");
+    zipCode.innerText = Object.zip_code;
+
+    const date = document.createElement("li");
+    date.innerText = Object.inspection_date;
+
+    const initial = document.createElement("li");
+    initial.innerText = Object.inspection_type;
+
+    const iResult = document.createElement("li");
+    iResult.innerText = Object.result;
+
+    const resultContainer = document.createElement("ul")
+    resultContainer.classList.add('resultContainer');
+
+    resultContainer.append(number, address, zipCode, date, initial, iResult);
+
+    document.querySelector('.inspectionResults').appendChild('.resultContainer');
+
+  })
+}
+``
+
+
+
 
 app.events = () => { 
+ // Add eventListener on submit when user input address
+  document.querySelector("form").addEventListener('submit', function(e){
+    const userChoice = e.target.value;
+    app.getRecords(userChoice);
+    e.preventDefault();
+  });
+};
+ 
+ //ACCORDION MENU
   app.accordionItemHeader = document.querySelectorAll(".accordionItemHeader");
   app.accordionItemHeader.forEach(header => {
     
@@ -33,7 +93,6 @@ app.events = () => {
       }
     });
   });
-};
 
 
 app.init = () => {
